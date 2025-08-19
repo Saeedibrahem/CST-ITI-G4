0b288aee13c758ca272232d5b9714c02c890f0cd
Abdulrahim <abdulrahimsakr01@gmail.com>
2025-08-19 16:11:43 +0300

    update:main.js:fetch users method

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,8 @@
-async function loadUsers() {
-  try {
-    const response = await fetch("../../data/users.json");
-    if (!response.ok) {
-      throw new Error("Failed to load users.json");
-    }
-    const data = await response.json();
-    // Save users into localStorage
-    console.log(data);
-    localStorage.setItem("users", JSON.stringify(data));
-  } catch (err) {
-    console.log("Error loading users:", err);
-  }
-}
-// call-loadUsers-func
-loadUsers();
+// fetch users
+fetch("../../data/users.json")
+  .then((res) => res.json())
+  .then((users) => {
+    localStorage.setItem("users", JSON.stringify(users));
+  })
+  .catch((err) => console.error("Error loading users:", err));
+
